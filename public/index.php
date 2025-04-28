93f8a0f017f1bd0506f1ecb3d6379dcf1dead716
cjdoong <doongd97@gmail.com>
2025-04-28 15:25:38 +0800

    delete file

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ $page = $_GET['page'] ?? 'login';
 
 switch ($page) {
     case 'login':
+        
+        require_once '../database/config.php';
         require_once '../view/loginView.php';
         break;
 }
